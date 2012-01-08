53fbb54a1ed19d4b8d0e6bf8fa31eb3b58b5f11d
Richard Allen <rsaxvc@gmail.com>
2012-01-08 14:47:33 -0600

    \t

--- gpx2sqlite.py
+++ gpx2sqlite.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sqlite3,sys,os,random
 import xml.dom.minidom
+import zlib
 
 id = 0
 
@@ -10,8 +11,10 @@ if( len( sys.argv ) >= 3 ):
     c = conn.cursor()
 
     # Create tables
-    c.execute('''CREATE TABLE geocaches( id, gc_code VARCHAR(10), name TEXT, short_desc TEXT, long_desc TEXT, awesomeness REAL, difficulty REAL, size REAL, terrain REAL)''')
-    c.execute('''CREATE VIRTUAL TABLE geocaches_r USING rtree(id,minLat,maxLat,minLon,maxLon)''')
+    c.execute("CREATE TABLE geocaches                      ( id INTEGER PRIMARY KEY, gc_code VARCHAR(10), short_desc TEXT, long_desc BLOB, awesomeness REAL, difficulty REAL, size REAL, terrain REAL)")
+    c.execute("CREATE VIRTUAL TABLE geocaches_r USING rtree( id INTEGER PRIMARY KEY, minLat REAL,maxLat REAL, minLon REAL, maxLon REAL)")
+    c.execute("CREATE VIRTUAL TABLE geocache_t  USING fts4 ( id INTEGER PRIMARY KEY, name TEXT)")
+    c.execute("CREATE TABLE geocache_logs                 ( id INTEGER PRIMARY KEY, gc_id INT, type TEXT, finder TEXT, text BLOB)")
 
     for  file in sys.argv[ 1:-1]:
         print "Parsing:",file
@@ -63,10 +66,36 @@ if( len( sys.argv ) >= 3 ):
             terrain     = random.uniform(0,5)
 
             ## Insert a row of data
-            c.execute("""INSERT INTO geocaches   VALUES ( ?,?,?,?,?,?,?,?,? )""", (id, gccode, name,short_desc,long_desc,awesomeness,difficulty,size,terrain) )
+            long_desc.encode("utf-8")
+            zlib.compress( long_desc.encode("utf-8") )
+            sqlite3.Binary( zlib.compress( long_desc.encode("utf-8") ) )
+            c.execute("""INSERT INTO geocaches   VALUES ( ?,?,?,?,?,?,?,? )""", (id, gccode, short_desc, sqlite3.Binary( zlib.compress( long_desc.encode("utf-8" ) ) ),awesomeness,difficulty,size,terrain) )
             c.execute("""INSERT INTO geocaches_r VALUES ( ?,?,?,?,? )""", (id,lat,lat,lon,lon) )
+            c.execute("""INSERT INTO geocache_t  VALUES ( ?,?)""",(id,name) )
 
-        
+            for log_sets in geocache.getElementsByTagName("groundspeak:logs"):
+                for log in log_sets.getElementsByTagName("groundspeak:log"):
+                    log_id = log.getAttribute("id")
+
+                    log_type_tag = log.getElementsByTagName("groundspeak:type")
+                    if len( log_type_tag ) == 1:
+                        log_type = getText( log_type_tag[0].childNodes )
+                    else:
+                        log_type = ""
+
+                    log_finder_tag = log.getElementsByTagName("groundspeak:finder")
+                    if len( log_finder_tag ) == 1:
+                        log_finder = getText( log_finder_tag[0].childNodes )
+                    else:
+                        log_finder = ""
+
+                    log_text_tag = log.getElementsByTagName("groundspeak:text")
+                    if len( log_text_tag ) == 1:
+                        log_text = getText( log_text_tag[0].childNodes )
+                    else:
+                        log_text = ""
+                    print log_id
+                    c.execute("INSERT INTO geocache_logs VALUES(?,?,?,?,?)", ( log_id, id, log_type, log_finder, sqlite3.Binary( zlib.compress( log_text.encode("utf-8") ) ) ) )
 
         def handleGPX(gpx):
             global id
@@ -77,11 +106,9 @@ if( len( sys.argv ) >= 3 ):
         print "Packing:",file
         handleGPX(dom)
 
-    c.execute("VACUUM;")
     # Save (commit) the changes
     conn.commit()
-
-    # We can also close the cursor if we are done with it
+    c.execute("VACUUM;")
     c.close()
 else:
     print "Please run:%s [input.gpx]... output.db" % sys.argv[0]
