d2e833bab29edcfca93fd394e087e9a84be0235a
Richard Allen <rsaxvc@gmail.com>
2013-01-21 10:23:04 -0600

    minimal expat parser

---
+++ gpx_parse.py
@@ -0,0 +1,95 @@
+from xml.parsers import expat
+import sys
+
+class GchParser:
+
+	def __init__(self):
+		self._geocache=""
+
+	def feed(self, data):
+		self._parser.Parse(data, 0)
+
+	def close(self):
+		self._parser.Parse("", 1) # end of data
+		del self._parser # get rid of circular references
+
+	def start(self, tag, attrs, cbi):
+		print "GCH:Start", repr(tag), attrs, cbi
+
+	def end(self, tag, textbuffer, ebi):
+		print "GCH:End", repr(tag), textbuffer, ebi
+
+class NulParser:
+	def start(self, tag, attrs, cbi ):
+		print "NULL:Start"
+		pass
+
+	def end(self, tag, textbuffer, ebi):
+		print "NULL:End"
+		pass
+
+class GpxParser:
+	def __init__(self):
+		self._mode = "nul"
+		self._nul_parser = NulParser()
+		self._gch_parser = GchParser()
+		self._depth = 0
+		self._prev_byte_index = 0
+		self._cachelist = []
+		self._textbuffer = ""
+		self._parser = expat.ParserCreate()
+		self._parser.StartElementHandler = self.start
+		self._parser.EndElementHandler = self.end
+		self._parser.CharacterDataHandler = self.data
+
+	def feed(self, data ):
+		self._parser.Parse(data,0)
+
+	def close(self):
+		self._parser.Parse("", 1) # end of data
+		del self._parser # get rid of circular references
+
+	def start(self, tag, attrs):
+		if( tag == "wpt" ):
+			self._mode = "wpt"
+
+		self._textbuffer=""
+		self._depth = self._depth + 1
+		if( self._mode == "wpt" ):
+			self._gch_parser.start( tag, attrs, self._parser.CurrentByteIndex )
+		else:
+			self._nul_parser.start( tag, attrs, self._parser.CurrentByteIndex )
+		print "START", repr(tag), attrs, self._depth, self._parser.CurrentByteIndex
+
+	def data(self, data):
+		self._textbuffer += repr(data)
+
+	def end(self, tag):
+		print "END", repr(tag), self._textbuffer, self._depth
+		#To calculate the end pos, we need the CBI, which points to the start of the tag that caused
+		#the current event, and we need to add '</' , the taglen, and '>' to find the end-byte-index
+		end_pos = self._parser.CurrentByteIndex + 2 + len( tag ) + 1
+
+		if( self._mode == "wpt" ):
+			self._gch_parser.end( tag, self._textbuffer, end_pos )
+		else:
+			self._nul_parser.end( tag, self._textbuffer, end_pos )
+		
+		if( tag == "wpt" or tag == "rte" or tag == "trk" ):
+			self._mode = "nul"
+		self._depth = self._depth - 1
+
+
+if( len( sys.argv ) == 2 ):
+	p=GpxParser()
+	f = open( sys.argv[-1] )
+	p.feed( f.read() )
+	f.close()
+	p.close();
+else:
+    print "Please run:%s [input.gpx]" % sys.argv[0]
+
+
+
+
+
