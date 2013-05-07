1e7f286670924aa5bab79629138a07c9bc3cb5f5
Richard Allen <rsaxvc@gmail.com>
2013-05-06 22:39:38 -0500

    Add __str__ and __eq__ methods

--- geocache.py
+++ geocache.py
@@ -13,4 +13,11 @@ class Geocache:
 		self.type        = u""
 		self.found_status = ""
 
+	def __str__(self):
+		return str(self.__dict__)
+
+	def __eq__(self, other): 
+		return self.__dict__ == other.__dict__
+
+
 
